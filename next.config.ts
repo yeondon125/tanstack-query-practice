592b97e366bc3c717c9d26a13dec4e494b6acf6d
yeondon <s25030@gsm.hs.kr>
2025-12-27 16:17:18 +0900

    api 주소 추가

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: `https://jsonplaceholder.typicode.com/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
